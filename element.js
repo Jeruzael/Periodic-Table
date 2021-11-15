0dc808732abf416acce1499011fcbd175a9fdbef
level one <dumale.jeruzael.bscs2019@gmail.com>
2021-11-15 17:51:45 +0800

    periodic elements

---
+++ element.js
@@ -0,0 +1,11 @@
+function show(){
+
+	
+	document.getElementById("container").style.width = "1200px";
+	document.getElementById("container").style.height = "600px";
+	document.getElementById("container").style.marginLeft = "80px";
+	document.getElementById("container").style.marginTop = "20px";
+	document.getElementById("container").style.transition= "1s";
+	document.getElementById("imgContainer").style.width = "0px";
+	document.getElementById("imgContainer").style.height = "0px";
+}
